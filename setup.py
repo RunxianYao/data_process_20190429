370aba98894809ed5972c43927f6a2d3b7d4270e
RunxianYao <48881080+RunxianYao@users.noreply.github.com>
2020-04-30 20:14:42 +0800

    vina_screen_get_top.py

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-#coding=utf-8
-from distutils.core import setup
-setup(
-    name='data_process',
-    version='5.0',
-    author='安天池',
-    author_email='861710865@qq.com',
-    maintainer='安天池',
-    maintainer_email='xxx@qq.com',
-    url='www.eastcom.com',
-    packages=["","process","util"]
-)
+#! /usr/bin/env python
+
+import sys
+import glob
+
+def doit(n):
+    file_names = glob.glob('*/*.pdbqt')
+    everything = []
+    failures = []
+    print 'Found', len(file_names), 'pdbqt files'
+    for file_name in file_names:
+        file = open(file_name)
+        lines = file.readlines()
+        file.close()
+        try:
+            line = lines[1]
+            result = float(line.split(':')[1].split()[0])
+            everything.append([result, file_name])
+        except:
+            failures.append(file_name)
+    everything.sort(lambda x,y: cmp(x[0], y[0]))
+    part = everything[:n]
+    for p in part:
+        print p[1],
+    print
+    if len(failures) > 0:
+        print 'WARNING:', len(failures), 'pdbqt files could not be processed'
+
+if __name__ == '__main__':
+    doit(int(sys.argv[1]))
